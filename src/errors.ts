42cf101e69b22be10471bcf45e2fdc04b8472862
Thomas Grey <tgreyuk@gmail.com>
2017-11-23 22:50:30 +0000

    Simplified module (removed Class keyword)

--- src/errors.ts
+++ src/errors.ts
@@ -1,8 +1,6 @@
 /**
  * Error messages
  */
-export class Errors {
-  public static GAME_UPDATING = 'The game is being updated';
-  public static NOT_FOUND = 'Endpoint not found';
-  public static NO_RESPONSE = 'No response received from fpl';
-}
+export const GAME_UPDATING = 'The game is being updated';
+export const NOT_FOUND = 'Endpoint not found';
+export const NO_RESPONSE = 'No response received from fpl';
